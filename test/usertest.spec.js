6889efd1eb3731bbe1803b6fa29503a777fae132
robert <robertishimwe0@gmail.com>
2022-03-17 23:49:36 +0200

    setup unity tests

---
+++ test/usertest.spec.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/app.js';
+
+chai.should();
+
+chai.use(chaiHttp);
+
+const user = {
+    username: 'Johnny',
+    email: 'john.doe@andela.com',
+    firstName: 'John',
+    lastName: 'Doe',
+};
+
+describe('GET ALL USERS', () => {
+    //get all posts
+
+    describe('GET /api/v2/users', () => {
+        it('It Should Fetch all Posts', (done) => {
+            chai
+                .request(app)
+                .get('/api/v2/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                })
+                .timeout(10000);
+        });
+    });
+});
